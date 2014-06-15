616a3a1384948b7c1faf33ec3bfd9ce0fc55455e
Emmanuel Silva <portuga@Boo-2.local>
2014-06-15 18:56:36 -0300

    Initial Commit

---
+++ DataLogger/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DataLogger
+//
+//  Created by Emmanuel Silva on 6/15/14.
+//  Copyright (c) 2014 Emmanuel Silva. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
